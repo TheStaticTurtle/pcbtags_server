a0e9a52cf36f470ab1a1fa97625999618331d931
TheStaticTurtle <tuglersamuel@gmail.com>
2022-08-12 05:13:34 +0200

    [SPOTIFY] Correction of the option name

--- generators/spotify/__init__.py
+++ generators/spotify/__init__.py
@@ -6,7 +6,7 @@ AVAILABLE_CANVASES = [
 	{"key": "keychain", "text": "Keychain"}
 ]
 OPTIONS = [
-	{"type": "String", "key": "code", "text": "Music / Playlist URI", "default": "spotify:track:4cOdK2wGLETKBW3PvgPWqT"},
+	{"type": "String", "key": "code", "text": "Spotify URI / Share URL", "default": "spotify:track:4cOdK2wGLETKBW3PvgPWqT"},
 	{"type": "Boolean", "key": "draw_spotify_logo", "text": "Draw spotify logo", "default": True},
 	{"type": "Boolean", "key": "add_keychain_hole", "text": "Add the keychain hole", "default": True}
 ]
