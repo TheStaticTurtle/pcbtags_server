3db24c8cc72d48d182e15b047de89d874b6ad8a1
Samuel <tuglersamuel@gmail.com>
2022-08-15 00:57:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ VERSION = f"1.0.0-{tools.git_tool.get_last_commit_short_hash()}"
 
 CACHE_TIME_MINUTES = os.getenv('CACHE_TIME_MINUTES', 1440)
 
-REDIS_HOST = os.getenv('REDIS_HOST', 'pcbtags-server')
+REDIS_HOST = os.getenv('REDIS_HOST', 'pcbtags-cache')
 REDIS_PORT = os.getenv('REDIS_PORT', 6379)
 REDIS_DB = os.getenv('REDIS_DB', 0)
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
